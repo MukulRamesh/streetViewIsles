cde0ab81092914346a728c9f553b9fa77e3c6607
DiskMonkey <MukulPlace@gmail.com>
2024-08-01 10:57:48 -0400

    oop

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ safeLoc = urllib.parse.quote_plus("18 CLARK ST Trenton")
 
 size = "6000x3000"
 location = safeLoc
-key = "AIzaSyC5hgQGagiwSuiLlRghHxDisI__P11jGbk"
+key = open("key.txt", 'r').readline()
 
 url = f"https://maps.googleapis.com/maps/api/streetview?size={size}&location={location}&key={key}"
 
