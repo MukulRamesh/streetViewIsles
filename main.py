3723bdd944047ee4dfe45cdd6efcaf9a631bb781
DiskMonkey <MukulPlace@gmail.com>
2024-08-01 14:56:56 -0400

    testing

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import urllib.parse
+from PIL import Image
+import requests
+from io import BytesIO
+import json
 
-safeLoc = urllib.parse.quote_plus("18 CLARK ST Trenton")
 
+searchLoc = "18 CLARK ST Trenton"
 
-size = "6000x3000"
+safeLoc = urllib.parse.quote_plus(searchLoc)
+
+size = "6000x3000" #this just returns a nice quality image. the actual 
+
 location = safeLoc
-key = open("key.txt", 'r').readline()
+with open("key.txt", 'r') as file:
+	key = file.readline()
 
+
+
+metaURL = f"https://maps.googleapis.com/maps/api/streetview/metadata?size={size}&location={location}&key={key}"
 url = f"https://maps.googleapis.com/maps/api/streetview?size={size}&location={location}&key={key}"
 
-print(url)
+metaResponse = requests.get(metaURL)
+metaJSON = metaResponse.json()
+
+if (metaJSON['status'] != 'OK'):
+	print(f"Failed to get image for location '{searchLoc}'.")
+else:
+	date = metaJSON.get("date")
+	response = requests.get(url)
+	img = Image.open(BytesIO(response.content))
+
+	print(date)
+	img.save("./output.png")
